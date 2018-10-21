570a2861675f7096446e5a496898dbde415ac598
Liam Galvin <liam@liam-galvin.co.uk>
2018-10-21 16:37:08 +0100

    Failing test #2

--- main_test.go
+++ main_test.go
@@ -8,15 +8,21 @@ import (
 	"testing"
 	"time"
 
+	"github.com/satori/go.uuid"
+
+	"github.com/go-pg/pg/orm"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/go-pg/pg"
 )
 
 var server *http.Server
+var db *pg.DB
 
 func TestMain(m *testing.M) {
-	db := pg.Connect(&pg.Options{
+	db = pg.Connect(&pg.Options{
 		User:     "form3",
 		Password: "form3",
 		Addr:     "database:5432",
@@ -32,6 +38,10 @@ func TestMain(m *testing.M) {
 		break
 	}
 
+	if err := db.CreateTable(&Payment{}, &orm.CreateTableOptions{}); err != nil {
+		panic(err)
+	}
+
 	server = &http.Server{Addr: ":8080", Handler: newAPI(nil)}
 	code := m.Run()
 
@@ -39,6 +49,10 @@ func TestMain(m *testing.M) {
 }
 
 func TestGetPaymentsWithEmptyTable(t *testing.T) {
+
+	if _, err := db.Model(&Payment{}).Where("1=1").Delete(); err != nil {
+		t.Fatalf("Failed to empty table: %s", err)
+	}
 
 	req := httptest.NewRequest(http.MethodGet, "/v1/payments", nil)
 	rw := httptest.NewRecorder()
@@ -63,4 +77,40 @@ func TestGetPaymentsWithEmptyTable(t *testing.T) {
 	}
 
 	assert.Len(t, payments, 0, "Payments array must be empty when database is empty")
+}
+
+func TestGetPaymentsWithOneExistingPayment(t *testing.T) {
+
+	newID := uuid.NewV1()
+
+	// populate table with example payment
+	examplePayment := Payment{
+		ID: newID,
+	}
+	db.Insert(&examplePayment)
+
+	req := httptest.NewRequest(http.MethodGet, "/v1/payments", nil)
+	rw := httptest.NewRecorder()
+	server.Handler.ServeHTTP(rw, req)
+	if rw.Code != 200 {
+		t.Fatalf("Status code was not 200: %d\n", rw.Code)
+	}
+
+	if rw.Header().Get("Content-Type") != "application/json" {
+		t.Fatalf("Content type was not application/json")
+	}
+
+	var response APIResponse
+	err := json.NewDecoder(rw.Body).Decode(&response)
+	if err != nil {
+		t.Fatalf("Failed to decode API response: %s", err)
+	}
+
+	var payments []Payment
+	if err := json.Unmarshal(response.Data, &payments); err != nil {
+		t.Fatalf("Failed to decode response to payments slice: %s", err)
+	}
+
+	require.Len(t, payments, 1, "Payments array must contain one payment when database has one payment")
+	assert.Equal(t, newID, payments[0].ID)
 }
--- model_payment.go
+++ model_payment.go
@@ -1,4 +1,14 @@
 package main
 
+import uuid "github.com/satori/go.uuid"
+
 type Payment struct {
+	Type           string     `json:"type"`
+	ID             uuid.UUID  `json:"id"`
+	Version        uint       `json:"version"`
+	OrganisationID uuid.UUID  `json:"organisation_id"`
+	Attributes     Attributes `json:"attributes"`
+}
+
+type Attributes struct {
 }
